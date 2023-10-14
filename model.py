3d3172653bfe23daf307b6faf032d0627f5d0f21
Celina <60200600+cia161@users.noreply.github.com>
2023-10-14 15:21:42 -0500

    about page and modules

--- model.py
+++ model.py
@@ -1,40 +1,23 @@
 from roboflow import Roboflow
 import streamlit as st
-from streamlit_webrtc import webrtc_streamer, VideoProcessorBase, VideoTransformerBase
 
-custom_css = """
-<style>
-    .my-custom-class {
-        color: red;
-        background-color: lightgrey;
-        padding: 10px;
-    }
+def model():
+    st.title('ASL detection')
 
-    #my-custom-id {
-        font-size: 20px;
-    }
-</style>
-"""
-st.write(custom_css, unsafe_allow_html=True)
+    # Roboflow integration and ASL detection code
+    rf = Roboflow(api_key="07xd4Fb9Jmy0RHi57x5x")
+    project = rf.workspace().project("american-sign-language-letters")
+    model = project.version(6).model
 
-st.write('<div class="my-custom-class">This is styled with custom CSS</div>', unsafe_allow_html=True)
+    # Function to predict ASL signs
+    def predict(processed_image):
+        print(model.predict(processed_image, confidence=40, overlap=30).json())
 
-st.title('ASL detection')
+    picture = st.camera_input("Take a picture")
 
-rf = Roboflow(api_key="07xd4Fb9Jmy0RHi57x5x")
-project = rf.workspace().project("american-sign-language-letters")
-model = project.version(6).model
+    with open("captured_image.jpg", "wb") as f:
+        f.write(picture.getbuffer())
 
+    st.write('Image captured and saved as captured_image.jpg')
 
-# infer on a local image
-def predict(processed_image):
-    print(model.predict(processed_image, confidence=40, overlap=30).json())
-
-picture = st.camera_input("Take a picture")
-
-with open("captured_image.jpg", "wb") as f:
-    f.write(picture.getbuffer())
-
-st.write('Image captured and saved as captured_image.png')
-
-predict("captured_image.jpg")
+    predict("captured_image.jpg")
