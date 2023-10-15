abbcf4a5e720fa508cb04d70f58260d627636a02
nicolehu26 <nicolehu2626@gmail.com>
2023-10-15 11:28:51 -0500

    resource redirection

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import streamlit as st
 from about import about
+from resources import resources
 from model import model
+
 
 def main():
     st.sidebar.title("Navigation")
     st.sidebar.markdown("---")
 
     # Create custom navigation buttons for both "About" and "Model"
-    selected_page = st.sidebar.radio(" ", ["About", "Model"])
+    selected_page = st.sidebar.radio(" ", ["About", "Model", "Resources"])
     if selected_page == "About":
         about()
     elif selected_page == "Model":
         model()
+    elif selected_page == "Resources":
+        resources()
 
 if __name__ == "__main__":
     main()
